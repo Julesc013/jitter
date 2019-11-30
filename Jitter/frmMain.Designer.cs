880baf702a7896c07779be8b1700edfe26ce1197
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2019-12-01 04:23:39 +1100

    final changes made

--- Jitter/frmMain.Designer.cs
+++ Jitter/frmMain.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnStart = new System.Windows.Forms.Button();
             this.txtIP = new System.Windows.Forms.TextBox();
             this.dataGridView = new System.Windows.Forms.DataGridView();
@@ -38,30 +38,42 @@
             this.colIP = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.chartResponseTimes = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.numFrequency = new System.Windows.Forms.NumericUpDown();
-            this.numDuration = new System.Windows.Forms.NumericUpDown();
+            this.numInterval = new System.Windows.Forms.NumericUpDown();
+            this.numIterations = new System.Windows.Forms.NumericUpDown();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblIterations = new System.Windows.Forms.Label();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.lblMinimum = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblMaximum = new System.Windows.Forms.Label();
+            this.lblJitter = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartResponseTimes)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numFrequency)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numDuration)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numInterval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numIterations)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(44, 67);
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(323, 12);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.Size = new System.Drawing.Size(203, 46);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(44, 25);
+            this.txtIP.Location = new System.Drawing.Point(66, 12);
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(195, 20);
+            this.txtIP.Size = new System.Drawing.Size(251, 20);
             this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "www.google.com";
             // 
             // dataGridView
             // 
@@ -73,10 +85,10 @@
             this.colDomain,
             this.colIP,
             this.colTime});
-            this.dataGridView.Location = new System.Drawing.Point(12, 139);
+            this.dataGridView.Location = new System.Drawing.Point(12, 64);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
-            this.dataGridView.Size = new System.Drawing.Size(603, 181);
+            this.dataGridView.Size = new System.Drawing.Size(605, 285);
             this.dataGridView.TabIndex = 3;
             // 
             // colDomain
@@ -102,71 +114,182 @@
             // 
             // chartResponseTimes
             // 
-            chartArea1.AxisX.LabelStyle.Enabled = false;
-            chartArea1.AxisX.MaximumAutoSize = 100F;
-            chartArea1.AxisX.Title = "Pings Performed";
-            chartArea1.AxisY.MaximumAutoSize = 100F;
-            chartArea1.AxisY.Title = "Response Time (ms)";
-            chartArea1.Name = "ChartArea1";
-            this.chartResponseTimes.ChartAreas.Add(chartArea1);
-            legend1.Enabled = false;
-            legend1.Name = "Legend1";
-            this.chartResponseTimes.Legends.Add(legend1);
-            this.chartResponseTimes.Location = new System.Drawing.Point(12, 326);
+            chartArea3.AxisX.LabelStyle.Enabled = false;
+            chartArea3.AxisX.MaximumAutoSize = 100F;
+            chartArea3.AxisX.Title = "Responses Recieved";
+            chartArea3.AxisY.MaximumAutoSize = 100F;
+            chartArea3.AxisY.Title = "Response Time (ms)";
+            chartArea3.Name = "ChartArea1";
+            this.chartResponseTimes.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.chartResponseTimes.Legends.Add(legend3);
+            this.chartResponseTimes.Location = new System.Drawing.Point(12, 403);
             this.chartResponseTimes.Name = "chartResponseTimes";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
-            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            series1.Legend = "Legend1";
-            series1.Name = "Response Time";
-            series1.YValuesPerPoint = 4;
-            this.chartResponseTimes.Series.Add(series1);
-            this.chartResponseTimes.Size = new System.Drawing.Size(603, 199);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series3.Color = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            series3.Legend = "Legend1";
+            series3.Name = "Response Times";
+            series3.YValuesPerPoint = 4;
+            this.chartResponseTimes.Series.Add(series3);
+            this.chartResponseTimes.Size = new System.Drawing.Size(605, 187);
             this.chartResponseTimes.TabIndex = 4;
             this.chartResponseTimes.Text = "Response Times";
             // 
-            // numFrequency
+            // numInterval
             // 
-            this.numFrequency.Location = new System.Drawing.Point(372, 51);
-            this.numFrequency.Maximum = new decimal(new int[] {
+            this.numInterval.Increment = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.numInterval.Location = new System.Drawing.Point(227, 38);
+            this.numInterval.Maximum = new decimal(new int[] {
             100000000,
             0,
             0,
             0});
-            this.numFrequency.Name = "numFrequency";
-            this.numFrequency.Size = new System.Drawing.Size(120, 20);
-            this.numFrequency.TabIndex = 5;
+            this.numInterval.Name = "numInterval";
+            this.numInterval.Size = new System.Drawing.Size(90, 20);
+            this.numInterval.TabIndex = 5;
+            this.numInterval.Value = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
             // 
-            // numDuration
+            // numIterations
             // 
-            this.numDuration.Location = new System.Drawing.Point(372, 25);
-            this.numDuration.Maximum = new decimal(new int[] {
+            this.numIterations.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numIterations.Location = new System.Drawing.Point(66, 38);
+            this.numIterations.Maximum = new decimal(new int[] {
             1000000000,
             0,
             0,
             0});
-            this.numDuration.Minimum = new decimal(new int[] {
+            this.numIterations.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numDuration.Name = "numDuration";
-            this.numDuration.Size = new System.Drawing.Size(120, 20);
-            this.numDuration.TabIndex = 6;
-            this.numDuration.Value = new decimal(new int[] {
-            1,
+            this.numIterations.Name = "numIterations";
+            this.numIterations.Size = new System.Drawing.Size(90, 20);
+            this.numIterations.TabIndex = 6;
+            this.numIterations.Value = new decimal(new int[] {
+            10,
             0,
             0,
             0});
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClear.Location = new System.Drawing.Point(532, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 46);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(323, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(203, 46);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Visible = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(12, 15);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(48, 13);
+            this.lblIP.TabIndex = 9;
+            this.lblIP.Text = "Address:";
+            // 
+            // lblIterations
+            // 
+            this.lblIterations.AutoSize = true;
+            this.lblIterations.Location = new System.Drawing.Point(12, 40);
+            this.lblIterations.Name = "lblIterations";
+            this.lblIterations.Size = new System.Drawing.Size(53, 13);
+            this.lblIterations.TabIndex = 10;
+            this.lblIterations.Text = "Iterations:";
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Location = new System.Drawing.Point(162, 40);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(59, 13);
+            this.lblInterval.TabIndex = 11;
+            this.lblInterval.Text = "Delay (ms):";
+            // 
+            // lblMinimum
+            // 
+            this.lblMinimum.Location = new System.Drawing.Point(12, 358);
+            this.lblMinimum.Name = "lblMinimum";
+            this.lblMinimum.Size = new System.Drawing.Size(180, 13);
+            this.lblMinimum.TabIndex = 12;
+            this.lblMinimum.Text = "Minimum: N/A";
+            this.lblMinimum.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.Location = new System.Drawing.Point(224, 358);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(180, 13);
+            this.lblAverage.TabIndex = 13;
+            this.lblAverage.Text = "Average: N/A";
+            this.lblAverage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblMaximum
+            // 
+            this.lblMaximum.Location = new System.Drawing.Point(437, 358);
+            this.lblMaximum.Name = "lblMaximum";
+            this.lblMaximum.Size = new System.Drawing.Size(180, 13);
+            this.lblMaximum.TabIndex = 14;
+            this.lblMaximum.Text = "Maximum: N/A";
+            // 
+            // lblJitter
+            // 
+            this.lblJitter.Location = new System.Drawing.Point(224, 379);
+            this.lblJitter.Name = "lblJitter";
+            this.lblJitter.Size = new System.Drawing.Size(180, 13);
+            this.lblJitter.TabIndex = 15;
+            this.lblJitter.Text = "Jitter: N/A";
+            this.lblJitter.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // frmMain
             // 
             this.AcceptButton = this.btnStart;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1015, 647);
-            this.Controls.Add(this.numDuration);
-            this.Controls.Add(this.numFrequency);
+            this.ClientSize = new System.Drawing.Size(629, 602);
+            this.Controls.Add(this.lblJitter);
+            this.Controls.Add(this.lblMaximum);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblMinimum);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.lblIterations);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.numIterations);
+            this.Controls.Add(this.numInterval);
             this.Controls.Add(this.chartResponseTimes);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.txtIP);
@@ -177,8 +300,8 @@
             this.Text = "Jitter";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartResponseTimes)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numFrequency)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numDuration)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numInterval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numIterations)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,8 +316,17 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colIP;
         private System.Windows.Forms.DataGridViewTextBoxColumn colTime;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartResponseTimes;
-        private System.Windows.Forms.NumericUpDown numFrequency;
-        private System.Windows.Forms.NumericUpDown numDuration;
+        private System.Windows.Forms.NumericUpDown numInterval;
+        private System.Windows.Forms.NumericUpDown numIterations;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblIterations;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.Label lblMinimum;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblMaximum;
+        private System.Windows.Forms.Label lblJitter;
     }
 }
 
--- Jitter/frmMain.cs
+++ Jitter/frmMain.cs
@@ -8,51 +8,152 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net.NetworkInformation;
+using System.Timers;
 
 namespace Jitter
 {
     public partial class frmMain : Form
     {
+
+        bool run; // Only run loop when this is true.
+
         public frmMain()
         {
             InitializeComponent();
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        private async void btnStart_Click(object sender, EventArgs e)
         {
+
+            int iterations = Convert.ToInt32(numIterations.Value);
+            int interval = Convert.ToInt32(numInterval.Value); // Get the desired wait interval.
+
+            // Enable stop button if the task will take a long time.
+            if (iterations >= 20 || interval >= 20 || iterations * interval >= 20)
+            {
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                btnStop.Visible = true;
+                btnStart.Visible = false;
+            }
+
+            // Initialise a new ping object.
+            Ping ping = new Ping();
+            PingReply reply;
+            // Get address to ping.
+            string inputRaw = txtIP.Text;
+            string[] inputArray = inputRaw.Split(Convert.ToChar("/"));
+            string input = inputArray[0];
 
             try
             {
 
-                // Initialise a new ping object.
-                Ping ping = new Ping();
-                PingReply reply;
-                string input;
-                input = txtIP.Text;
-                reply = ping.Send(input);
+                run = true; // Enable loop.
 
-                if (reply.Status == IPStatus.Success) // If the ping was successful.
+                while (iterations != 0 && run == true) // Ping for the number of iterations specified.
                 {
-                    // Add reply data to the table.
-                    dataGridView.Rows.Add(input.ToString(), reply.Address.ToString(), reply.RoundtripTime.ToString());
-                }
 
+                    reply = ping.Send(input);
+
+                    if (reply.Status == IPStatus.Success) // If the ping was successful.
+                    {
+
+                        // Get response values.
+                        string address = reply.Address.ToString();
+                        string responseTime = reply.RoundtripTime.ToString();
+
+                        // Add reply data to the table and chart.
+                        dataGridView.Rows.Add(input, address, responseTime);
+                        chartResponseTimes.Series["Response Times"].Points.AddY(responseTime);
+
+                        // Scroll to latest row.
+                        try
+                        {
+                            dataGridView.FirstDisplayedScrollingRowIndex += 1;
+                        }
+                        catch { }
+                    }
+
+                    // Get average response time and other statistical values.
+                    int[] responseTimes = (from DataGridViewRow row in dataGridView.Rows
+                                        where row.Cells[2].FormattedValue.ToString() != string.Empty
+                                        select Convert.ToInt32(row.Cells[2].FormattedValue)).ToArray();
+                    int responseTimesLength = responseTimes.Length;
+
+                    // Declare values for statistics and set them.
+                    int pingMinimum = responseTimes.Min();
+                    int pingMaximum = responseTimes.Max();
+                    int pingAverage = Convert.ToInt32(responseTimes.Average());
+                    int pingChange = 0;
+                    int jitter = 0;
+
+                    try
+                    {
+
+                        //Calculate jitter.
+                        for (int i = 1; i < responseTimesLength; i++)
+                        {
+                            // Get the total change (in ms) between each ping)
+                            pingChange += Math.Abs(responseTimes[i] - responseTimes[i - 1]);
+                        }
+
+                        jitter = pingChange / (responseTimesLength - 1); //Divide the total change by the number of changes.
+
+                    }
+                    catch (DivideByZeroException) { }
+
+                    // Populate text boxes.
+                    lblMinimum.Text = "Minimum: " + pingMinimum.ToString() + " ms"; // Min Value.
+                    lblMaximum.Text = "Maximum: " + pingMaximum.ToString() + " ms"; // Max Value.
+                    lblAverage.Text = "Average: " + pingAverage.ToString() + " ms"; // Average Value.
+                    lblJitter.Text = "Jitter: " + jitter.ToString() + " Δms"; // Jitter Value.
+
+
+                    await Task.Delay(interval); // Wait for the prescribed interval.
+                    iterations -= 1; // Decrement iterations to go.
+
+                }
+                
             }
 
             catch (ArgumentNullException)
             {
-
                 MessageBox.Show("Please use valid IP or web address.", "Invalid Address", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
             }
             catch (PingException ex)
             {
-
                 MessageBox.Show(ex.Message, "Ping Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
             }
+
+            // Re-enable buttons.
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnStop.Visible = false;
+            btnStart.Visible = true;
 
         }
 
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+
+            // Clear existing data in table and chart.
+            dataGridView.Rows.Clear();
+            chartResponseTimes.Series["Response Times"].Points.Clear();
+
+            // Reset text boxes.
+            lblMinimum.Text = "Minimum: N/A"; // Min Value.
+            lblMaximum.Text = "Maximum: N/A"; // Max Value.
+            lblAverage.Text = "Average: N/A"; // Average Value.
+            lblJitter.Text = "Jitter: N/A"; // Jitter Value.
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            // Interrupt loop.
+            run = false;
+
+        }
     }
 }
