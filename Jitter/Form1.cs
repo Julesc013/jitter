5bbf2b1e4052ddf30cfd2f76ec1acd554161dffd
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2019-12-01 10:43:36 +1100

    initial commit

---
+++ Jitter/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace Jitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ping p = new Ping();
+            PingReply r;
+            string s;
+            s = textBox1.Text;
+            r = p.Send(s);
+
+            if (r.Status == IPStatus.Success)
+            {
+                lblResult.Text = "Ping to " + s.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
+                   + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
+            }
+        }
+
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || textBox1.Text == "")
+            {
+                MessageBox.Show("Please use valid IP or web address!!");
+            }
+        }
+    }
+}
